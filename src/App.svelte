a13afaa0bc24d27c3ffd616216a3a0494cf4cd93
KuroTenshi <margot.pellegrin@gmail.com>
2024-03-21 16:53:50 +0100

    building process commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,29 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import PhotoCV_circle_rogne from '../public/PhotoCV_circle_rogne.png';
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <img class="logo" src={PhotoCV_circle_rogne} alt="photo"/>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+  <h1>Margot Pellegrin Portfolio</h1>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Welcome to my portfolio ! I'm Margot Pellegrin a student in IT development.
   </p>
 
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    This site is in building process, please visit it in few days.
   </p>
 </main>
 
 <style>
   .logo {
-    height: 6em;
+    height: 15em;
     padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
   }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
   }
   .read-the-docs {
     color: #888;
